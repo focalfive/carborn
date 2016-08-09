721299125886dbfe533e873d6b081b279b4f1942
slowslipper <slowslipper@gmail.com>
2016-08-09 18:18:24 +0900

    Apply design

--- ios/Carborn/CarListViewController.swift
+++ ios/Carborn/CarListViewController.swift
@@ -9,35 +9,96 @@
 import UIKit
 import RealmSwift
 
-class CarListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class CarListViewController: UIViewController, UIScrollViewDelegate {
     
-    let listView = UITableView()
+    var makerSize = CGSizeMake(100, 100)
+    var carSize = CGSizeMake(100, 100)
+    let makerListView = UIScrollView()
+    let carListView = UIScrollView()
 	let dbResults: Results<Car> = try! Realm().objects(Car).sorted("maker", ascending: true)
     var collection: Dictionary<String, [Car]> = Dictionary<String, [Car]>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.automaticallyAdjustsScrollViewInsets = false
+        
         for model in self.dbResults {
             let maker = model.maker
             if self.collection.keys.indexOf(maker) == nil {
 				self.collection[maker] = [Car]()
-//				print(maker)
             }
             self.collection[maker]!.append(model)
         }
         
-        self.listView.dataSource = self
-        self.listView.delegate = self
-        
         self.view.backgroundColor = UIColor.whiteColor()
         
         let viewSize = self.view.bounds.size
+        self.makerSize.width = viewSize.width
         var frame = CGRectZero
-        frame.size = viewSize
+        let headerHeight = UIApplication.sharedApplication().statusBarFrame.size.height + self.navigationController!.navigationBar.bounds.size.height
         
-        self.listView.frame = frame
-        self.view.addSubview(self.listView)
+        // Maker list
+        frame.origin.x = (viewSize.width - self.makerSize.width) * 0.5
+        frame.origin.y = headerHeight
+        frame.size.width = self.makerSize.width
+        frame.size.height = self.makerSize.height
+        
+        self.makerListView.frame = frame
+        self.makerListView.pagingEnabled = true
+        self.makerListView.showsVerticalScrollIndicator = false
+        self.makerListView.showsHorizontalScrollIndicator = false
+        self.makerListView.delegate = self
+        self.view.addSubview(self.makerListView)
+        
+        // Car list
+//        frame.origin.x = (viewSize.width - self.carSize.width) * 0.5
+        frame.origin.y += 20
+//        frame.size.width = self.carSize.width
+        frame.size.width = viewSize.width
+        frame.size.height = self.carSize.height
+        
+        self.carListView.frame = frame
+//        self.carListView.pagingEnabled = true
+        self.carListView.showsVerticalScrollIndicator = false
+        self.carListView.showsHorizontalScrollIndicator = false
+        self.carListView.delegate = self
+        self.view.addSubview(self.carListView)
+        
+        self.initData()
+    }
+    
+    func initData() {
+        initMakerData()
+        initCarData()
+    }
+    
+    func initMakerData() {
+        var contentWidth = CGFloat(0)
+        for makerName in self.collection.keys {
+            let makerLabel = UILabel(frame: CGRectMake(contentWidth, 0, self.makerSize.width, self.makerSize.height))
+            makerLabel.textAlignment = .Center
+            makerLabel.text = makerName
+            self.makerListView.addSubview(makerLabel)
+            contentWidth += self.makerSize.width
+        }
+        
+        self.makerListView.contentSize = CGSizeMake(contentWidth, self.makerSize.height)
+    }
+    
+    func initCarData() {
+        var contentWidth = CGFloat(0)
+        for (_, cars) in self.collection {
+            for car in cars {
+                let carLabel = UILabel(frame: CGRectMake(contentWidth, 0, self.carSize.width, self.carSize.height))
+                carLabel.textAlignment = .Center
+                carLabel.text = car.model
+                self.carListView.addSubview(carLabel)
+                contentWidth += self.carSize.width
+            }
+        }
+        
+        self.makerListView.contentSize = CGSizeMake(contentWidth, self.carSize.height)
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,49 +106,8 @@ class CarListViewController: UIViewController, UITableViewDelegate, UITableViewD
         // Dispose of any resources that can be recreated.
     }
     
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return self.collection.keys.count
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        let key = Array(self.collection.keys).sort(<)[section]
-        return self.collection[key]!.count
-        
-    }
-    
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 50
-    }
-    
-    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        
-        let key = Array(self.collection.keys).sort(<)[section]
-        return key
-        
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let key = Array(self.collection.keys).sort(<)[indexPath.section]
-        let model = self.collection[key]![indexPath.row]
-        
-        let cell = UITableViewCell()
-        cell.textLabel!.text = model.model
-        
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-//        print("Select", indexPath.section, indexPath.row)
-		
-		let key = Array(self.collection.keys).sort(<)[indexPath.section]
-		let model = self.collection[key]![indexPath.row]
-		
-		let detailViewController = CarDetailViewController()
-		detailViewController.model = model
-		
-		self.navigationController?.pushViewController(detailViewController, animated: true)
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        print("scroll", scrollView.contentOffset)
     }
 
 }
