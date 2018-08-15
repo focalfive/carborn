b8b6514592394feb115e1ae67931ba46f7a5ac11
slowslipper <slowslipper@gmail.com>
2018-08-16 01:05:17 +0900

    Reset project

--- ios/Pods/Target Support Files/Pods-carbornTests/Pods-carbornTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_carbornTests : NSObject
-@end
-@implementation PodsDummy_Pods_carbornTests
-@end
--- ios/Pods/Target Support Files/Pods-carbornUITests/Pods-carbornUITests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_carbornUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_carbornUITestsVersionString[];
-
